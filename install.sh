c92418eec22a9d51396ce7179d4cf71b52ed345d
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2020-07-30 23:52:52 -0400

    Update stuff

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ DIR_DOTFILES_CONFIG=$DIR_DOTFILES/config
 
 xcode-select --install
 
+# Setup bash_profile
+#
+#   1. Download git-completion.bash and move it to ~/
+#   2. Copy bash_profile to ~/
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+mv git-completion.bash ~/.git-completion.bash
 cp $DIR_DOTFILES_CONFIG/.bash_profile $DIR_HOME
 source $DIR_HOME/.bash_profile
 
