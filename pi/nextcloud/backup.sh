f13350ee57e87d1a8f058fb20a05ff45ac6ab763
Kunal Nagar <knlnagar@gmail.com>
2022-01-17 15:02:40 -0500

    Add elapsed time to email

--- pi/nextcloud/backup.sh
+++ pi/nextcloud/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# today=$(date +"%Y_%m_%d")
+start_time=$(date +%s)
 config_path="/srv/dev-disk-by-label-Storage1/Config/nextcloud/"
 config_backup_path="/srv/dev-disk-by-label-Storage1/Backups/duplicati/nextcloud/config.tgz"
 db_path="/srv/dev-disk-by-label-Storage1/Databases/nextcloud"
@@ -12,5 +12,9 @@ sudo tar -cvjf ${db_backup_path} -C ${db_path} .
 # Config Backup
 sudo tar -cvjf ${config_backup_path} -C ${config_path} .
 
+end_time=$(date +%s)
+elapsed=$(( end_time - start_time ))
+elapsed_string=$(eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')")
+
 # Send email
-echo -e "Subject: raspberrypi - Backup - Nextcloud DONE\r\n\r\nThe backup was successful" | msmtp --from=default -t knlnagar@gmail.com
+echo -e "Subject: raspberrypi - Backup - Nextcloud DONE\r\n\r\nThe backup was successful.\n${elapsed_string}." | msmtp --from=default -t knlnagar@gmail.com
