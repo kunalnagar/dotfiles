098e608fb1aa8fa933838ae9021c53dd5f67aa1c
Kunal Nagar <knlnagar@gmail.com>
2022-01-28 12:55:00 -0500

    Add pihole, plex and syncthing

---
+++ pi/plex/README.md
@@ -0,0 +1,7 @@
+# Plex Backup
+
+This essentially backs up the Config:
+
+- config.tgz
+
+This script runs everyday at 1:00AM before the Duplicati job to backup runs at 3:00AM.
---
+++ pi/syncthing/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+start_time=$(date +%s)
+config_path="/srv/dev-disk-by-label-Storage1/Config/Syncthing/"
+config_backup_path="/srv/dev-disk-by-label-Storage1/Backups/duplicati/syncthing/config.tgz"
+
+# backup
+sudo tar -cvjf ${config_backup_path} -C ${config_path} .
+
+end_time=$(date +%s)
+elapsed=$(( end_time - start_time ))
+elapsed_string=$(eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')")
+
+# Send email
+echo -e "Subject: raspberrypi - Backup - Syncthing DONE\r\n\r\nThe backup was successful.\n${elapsed_string}." | msmtp --from=default -t knlnagar@gmail.com
