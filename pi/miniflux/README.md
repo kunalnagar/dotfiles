80774bf888c6381a51d05cc5897b010f612fd174
Kunal Nagar <knlnagar@gmail.com>
2022-01-28 12:42:01 -0500

    Add miniflux

---
+++ pi/miniflux/README.md
@@ -0,0 +1,7 @@
+# Miniflux Backup
+
+This essentially backs up the Config:
+
+- config.tgz
+
+This script runs everyday at 1:00AM before the Duplicati job to backup runs at 3:00AM.
---
+++ pi/miniflux/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+start_time=$(date +%s)
+config_path="/srv/dev-disk-by-label-Storage1/Config/miniflux/"
+config_backup_path="/srv/dev-disk-by-label-Storage1/Backups/duplicati/miniflux/config.tgz"
+
+# backup
+sudo tar -cvjf ${config_backup_path} -C ${config_path} .
+
+end_time=$(date +%s)
+elapsed=$(( end_time - start_time ))
+elapsed_string=$(eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')")
+
+# Send email
+echo -e "Subject: raspberrypi - Backup - Miniflux DONE\r\n\r\nThe backup was successful.\n${elapsed_string}." | msmtp --from=default -t knlnagar@gmail.com
