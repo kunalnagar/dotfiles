7a113ea8df869a1614009b9396ba6caeef3a4684
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2024-01-13 09:46:55 -0500

    feat: Add photoprism backup

---
+++ pi/photoprism/backup-proxmox.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+start_time=$(date +%s)
+
+# backup
+rm -rf /photoprism/backups/config
+rm -rf /photoprism/backups/config.tgz
+mkdir /photoprism/backups/config
+docker compose exec -T photoprism photoprism backup -i - > /photoprism/backups/config/db.sql
+tar -cvjf /photoprism/backups/config/originals.tgz /photoprism/originals
+tar -cvjf /photoprism/backups/config/storage.tgz /photoprism/storage
+tar -cvjf /photoprism/backups/config.tgz /photoprism/backups/config
+
+end_time=$(date +%s)
+elapsed=$(( end_time - start_time ))
+elapsed_string=$(eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')")
+
+echo $elapsed_string
