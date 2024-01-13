981651d0b776063543375dc071112e35a66c2398
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2024-01-13 00:24:24 -0500

    feat: Add MySQL backup to nextcloud

--- pi/nextcloud/backup-proxmox.sh
+++ pi/nextcloud/backup-proxmox.sh
@@ -8,6 +8,7 @@ rm -rf /backups/config.tgz
 mkdir /backups/config
 cp -r /var/www/nextcloud /backups/config/
 cp -r /var/www/nextcloud-data /backups/config/
+mysqldump --single-transaction -h $NEXTCLOUD_HOST -u $NEXTCLOUD_USER -p$NEXTCLOUD_MYSQL_ROOT_PASSWORD $NEXTCLOUD_DB > /backups/config/nextcloud-sql.bak
 tar -cvjf /backups/config.tgz /backups/config
 
 end_time=$(date +%s)
